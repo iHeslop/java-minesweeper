633127e87696c0f34beae5054da89444df756fae
iHeslop <iheslop@optusnet.com.au>
2024-04-29 22:23:12 +1000

    Added printGrid method

--- Board.java
+++ Board.java
@@ -1,3 +1,45 @@
-public class Board {
+import java.util.Random;
 
+public class Board {
+    private int width;
+    private int bombAmount;
+    private Square[][] grid;
+
+    public Board(int width, int bombAmount) {
+        this.width = width;
+        this.bombAmount = bombAmount;
+        this.grid = new Square[width][width];
+        createBoard();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Square[][] getGrid() {
+        return grid;
+    }
+
+    private void createBoard() {
+        // initialize board with empty grid of Squares
+        for (int i = 0; i < width; ++i) {
+            for (int j = 0; j < width; ++j) {
+                grid[i][j] = new Square();
+            }
+        }
+
+        // randomly place bombs within the grid
+        Random random = new Random();
+        int bombsPlaced = 0;
+        while (bombsPlaced < bombAmount) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(width);
+            if (!grid[x][y].hasBomb()) {
+                grid[x][y].setBomb(true);
+                bombsPlaced++;
+            }
+        }
+
+        // need to calculate adjacent bombs math here
+    }
 }
--- Minesweeper.java
+++ Minesweeper.java
@@ -1,5 +1,36 @@
 import java.util.Scanner;
 
 public class Minesweeper {
+    private Board board;
+    private Scanner scanner;
 
+    public Minesweeper(int width, int bombAmount) {
+        this.board = new Board(width, bombAmount);
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void playGame() {
+        printGrid();
+    }
+
+    private void printGrid() {
+        System.out.println("  A B C D E F G H I J");
+        for (int i = 0; i < board.getWidth(); i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < board.getWidth(); j++) {
+                Square square = board.getGrid()[i][j];
+                if (square.isRevealed()) {
+                    System.out.print(square.getAdjacentBombs() + " ");
+                } else {
+                    System.out.print("- ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Minesweeper game = new Minesweeper(10, 20);
+        game.playGame();
+    }
 }
